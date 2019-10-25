0468e188e0d83ad7d4b7322b9416ab1bbdff070b
Antti Tanskanen <antanskan@gmail.com>
2019-10-25 18:26:44 +0300

    configured for multiuser

--- routes/CRUD.js
+++ routes/CRUD.js
@@ -5,7 +5,9 @@ const db = require('../models/queries')
 
 router.get('/practises', db.getPractises);
 router.get('/practises/:id', db.getPractiseById);
+router.get('/userExists', db.userExists);
 router.post('/practises', db.createPractise);
+router.post('/createUser', db.createUser);
 router.put('/practises/:id', db.updatePractise);
 router.delete('/practises/:id', db.deletePractise);
 
